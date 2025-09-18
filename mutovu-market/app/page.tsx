15cfedcfa4558fcb4b6b2012c70d905e81b8e87f
np-leon <104311071+amin-leon@users.noreply.github.com>
2025-09-18 11:20:57 +0200

    landingpage -footer

--- mutovu-market/app/page.tsx
+++ mutovu-market/app/page.tsx
@@ -1,9 +1,11 @@
+import NextProFooter from "./components/home/Footer";
 import LandingPage from "./components/home/LandingPage";
 
 export default function Home() {
   return (
 <div>
     <LandingPage />
+    <NextProFooter />
 </div>
   );
 }
